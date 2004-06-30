017271a9b15df79dec5a9f622346e26983e3aa98
OHTSUKA Ko-hei <nene@kokogiko.net>
2004-06-30 09:36:00 +0000

    Import of KOKOGIKO/HTTP-MobileAgent-Ex-0.01022 from CPAN.
    gitpan-cpan-distribution: HTTP-MobileAgent-Ex
    gitpan-cpan-version:      0.01022
    gitpan-cpan-path:         KOKOGIKO/HTTP-MobileAgent-Ex-0.01022.tar.gz
    gitpan-cpan-author:       KOKOGIKO
    gitpan-cpan-maturity:     released

---
+++ lib/HTTP/MobileAgent/Ex/AirHPhoneData.pm
@@ -0,0 +1,21 @@
+package HTTP::MobileAgent::Ex::AirHPhoneData;
+
+use strict;
+use vars qw($DisplayMap);
+
+$DisplayMap ||= {
+  'AH-J3001V' => {
+    'width' => 128,
+    'height' => 130,
+    'depth' => 65536,
+    'color' => 1
+  },
+  'AH-K3001V' => {
+    'width' => 237,
+    'height' => 241,
+    'depth' => 262144,
+    'color' => 1
+  },
+};
+
+1;
